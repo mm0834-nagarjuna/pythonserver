82bcc80151d7d8b7c76ba83cec2ca4b801b86215
Posa Nagarjuna <mm0834@maven.local>
2024-06-28 12:57:19 +0530

    'fifthcommit'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 load_dotenv()
 
-app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG') == 'True'
 
 @app.route('/')
 def home():
@@ -16,17 +16,17 @@ def home():
 
 @app.route('/post', methods=['POST'])
 def post_example():
-    data = request.get_json()  
-     
+    data = request.get_json()
     return jsonify({"received_message": data})
 
-
 @app.route('/text_to_speech', methods=['POST'])
-def post_example():
-    data = request.get_json()  
+def text_to_speech():
+    data = request.get_json()
     text = data.get('text')
+    if not text:
+        return jsonify({"error": "No text provided"}), 400
     speak_text(text)
     return jsonify({"received_message": text})
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run(debug=app.config['DEBUG'])
